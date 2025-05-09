fdebd90c88cecb41fabf1d3439165f72623e8fab
nivya-2 <you@example.com>
2025-05-09 10:46:44 +0530

    feat:initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonDemoComponent } from './button-demo/button-demo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
