0288248df3eab94131a4623737b9578a1bc819ec
Dhunan <midhun2017.com>
2025-05-09 14:22:36 +0530

    Responsive header and used Prime NG

--- src/app/ui/profile/profile.component.ts
+++ src/app/ui/profile/profile.component.ts
@@ -1,8 +1,10 @@
 import { Component,Input } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [AvatarModule,AvatarGroupModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
