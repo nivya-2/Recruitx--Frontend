3e1194633d6c8381dba0819061112d8a2b83c929
Abhinand K <abhinandksasi@gmail.com>
2025-05-09 17:30:30 +0530

    feat:icon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { ButtonDemoComponent } from './button-demo/button-demo.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { ButtonComponent } from './ui/button/button.component';
 import { CardsComponent } from './ui/cards/cards.component';
+import { IconComponent } from './ui/icon/icon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonDemoComponent, HeaderComponent, ButtonComponent,CardsComponent],
+  imports: [RouterOutlet, ButtonDemoComponent,IconComponent
+,    HeaderComponent, ButtonComponent,CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
