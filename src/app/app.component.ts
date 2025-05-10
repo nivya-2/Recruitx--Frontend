84f6df93385080ce95404a07e3e7212ca858eb1a
Anjalik712 <anjali2sheeja@gmail.com>
2025-05-10 13:51:56 +0530

    feat:recruiter dashboard layout skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,11 @@ import { IconComponent } from './ui/icon/icon.component';
 import { IconGroupComponent } from './layouts/icon-group/icon-group.component';
 import { SidenavbarComponent } from './layouts/sidenavbar/sidenavbar.component';
 import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
+import { RecruiterDashboardComponent } from './pages/recruiter-dashboard/recruiter-dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SidenavbarComponent,IconGroupComponent, ButtonDemoComponent,IconComponent
+  imports: [RecruiterDashboardComponent,RouterOutlet,SidenavbarComponent,IconGroupComponent, ButtonDemoComponent,IconComponent
 ,    HeaderComponent, ButtonComponent,CardsComponent,SharedComponentsModule,TableComponent, CommonLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/pages/recruiter-dashboard/recruiter-dashboard.component.ts
+++ src/app/pages/recruiter-dashboard/recruiter-dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonLayoutComponent } from '../../layouts/common-layout/common-layout.component';
+import { CardsComponent } from '../../ui/cards/cards.component';
 
 @Component({
   selector: 'app-recruiter-dashboard',
-  imports: [],
+  imports: [CommonLayoutComponent,CardsComponent],
   templateUrl: './recruiter-dashboard.component.html',
   styleUrl: './recruiter-dashboard.component.scss'
 })
