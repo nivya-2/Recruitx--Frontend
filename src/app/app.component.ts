98a0ca5cc14595d5c09f50076c265f66643b947b
K_vinj <kevinjoby1234@gmail.com>
2025-05-12 10:36:00 +0530

    feat:Analytics Page Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ import { IconComponent } from './ui/icon/icon.component';
 import { IconGroupComponent } from './layouts/icon-group/icon-group.component';
 import { SidenavbarComponent } from './layouts/sidenavbar/sidenavbar.component';
 import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
+import { AnalyticsComponent } from "./pages/analytics/analytics.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SidenavbarComponent,IconGroupComponent, ButtonDemoComponent,IconComponent
-,    HeaderComponent, ButtonComponent,CardsComponent,SharedComponentsModule,TableComponent, CommonLayoutComponent],
+  imports: [RouterOutlet, SidenavbarComponent, IconGroupComponent, ButtonDemoComponent, IconComponent,
+    HeaderComponent, ButtonComponent, CardsComponent, SharedComponentsModule, TableComponent, CommonLayoutComponent, AnalyticsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
