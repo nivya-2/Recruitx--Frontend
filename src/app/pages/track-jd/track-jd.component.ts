3f3f9d4d99227e1ac6db867bd9aebe89b76daeb0
Dhunan <midhun2017.com>
2025-05-16 00:16:01 +0530

    Tabs added

--- src/app/pages/track-jd/track-jd.component.ts
+++ src/app/pages/track-jd/track-jd.component.ts
@@ -3,9 +3,11 @@ import { CommonLayoutComponent } from '../../layouts/common-layout/common-layout
 import { CardsComponent } from '../../ui/cards/cards.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TabsModule } from 'primeng/tabs';
+import { Router } from '@angular/router';
+import { TabsComponent } from '../../shared-components/tabs/tabs.component';
 @Component({
   selector: 'app-track-jd',
-  imports: [CommonLayoutComponent,RouterOutlet,RouterLink,RouterLinkActive,TabsModule],
+  imports: [CommonLayoutComponent,RouterOutlet,RouterLink,RouterLinkActive,TabsModule,TabsComponent],
   templateUrl: './track-jd.component.html',
   styleUrl: './track-jd.component.scss'
 })
@@ -14,13 +16,15 @@ rla3: any;
 tabPanel: any;
 rla1: any;
 rla2: any;
- tabs: { title: string; value: number; content: string }[] = [];
+ tabs: { title: string; value: number; route: string }[] = [];
+  selectedTabIndex = 0;
 
     ngOnInit() {
         this.tabs = [
-            { title: 'Tab 1', value: 0, content: 'Tab 1 Content' },
-            { title: 'Tab 2', value: 1, content: 'Tab 2 Content' },
-            { title: 'Tab 3', value: 2, content: 'Tab 3 Content' },
-        ];
+  { title: 'Pending JD Generation', value: 0, route: 'pendingjdgeneration' },
+  { title: 'Open JD', value: 1, route: 'openjd' },
+  { title: 'Closed JD', value: 2, route: 'closedjd' }
+];
+
     }
 }
