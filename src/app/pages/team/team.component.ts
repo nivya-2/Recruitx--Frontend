29260720635e1bd179cc97749dfa49030b25c892
Abhinand K <abhinandksasi@gmail.com>
2025-05-16 16:15:54 +0530

    team table edited

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonLayoutComponent } from '../../layouts/common-layout/common-layout.component';
 import { CardsComponent } from '../../ui/cards/cards.component';
 import { TabsComponent } from '../../shared-components/tabs/tabs.component';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-team',
-  imports: [CommonLayoutComponent, CardsComponent, TabsComponent],
+  imports: [CommonLayoutComponent, RouterOutlet,CardsComponent, TabsComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.scss',
 })
