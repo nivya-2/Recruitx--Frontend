3cf0c7a1abbc9792c620b0f33fe963067c0343b2
Abhinand K <abhinandksasi@gmail.com>
2025-05-20 11:23:55 +0530

    jr assign modal 90%

--- src/app/shared-components/jr-card/jr-card.component.ts
+++ src/app/shared-components/jr-card/jr-card.component.ts
@@ -1,41 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { AvatarModule } from 'primeng/avatar';
 import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
-import { NgClass } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { ButtonComponent } from "../../ui/button/button.component";
+import { ProfileBoxComponent } from "../../ui/profile-box/profile-box.component";
+
 
 
 
 @Component({
   selector: 'app-jr-card',
-  imports: [CardModule, AvatarModule, DropdownModule, ButtonModule, NgClass, ButtonComponent],
+  imports: [CardModule, AvatarModule,DropdownModule, ButtonModule,NgFor, NgClass, ButtonComponent, ProfileBoxComponent],
   templateUrl: './jr-card.component.html',
   styleUrl: './jr-card.component.scss'
 })
 export class JrCardComponent {
-  jobData = {
-    priority: 'Urgent',
-    raisedBy: {
-      initials: 'JV',
-      name: 'Jins K. Varghese',
-      role: 'Tech Lead'
-    },
-    title: 'Data Scientist - Computer Vision',
-    du: 'DU6 – Advanced AI & ML Solutions',
-    openPositions: 6,
-    raisedDate: 'April 2, 2025',
-    location: 'Kochi',
+  @Input() name: string = 'Arthur Pendragon';
+  @Input() role: string = 'Tech Lead';
+  jobDetails = {
+    jobTag:'Urgent',
     requisitionId: 'REQ–2025–DSCV–006',
     jobTitle: 'Data Scientist – Computer Vision',
+    deliveryUnit: 'DU6',
+    team:'Advanced AI & ML Solutions',
+    location: 'Kochi',
+    openPositions: 6,
+    raisedOn: 'April 2, 2025',
     hiringManager: 'Arjun Menon',
-    recruiterAssigned: 'Not Assigned',
+    recruiter: 'Not Assigned',
     qualifications: [
-      'Bachelor’s/Master’s degree in Computer Science, Data Science, or related field.',
-      '2+ years of hands-on experience in computer vision or a similar role.',
-      'Strong portfolio or GitHub with previous CV projects preferred.'
+      "Bachelor’s/Master’s degree in Computer Science, Data Science, or related field.",
+      "2+ years of hands-on experience in computer vision or a similar role.",
+      "Strong portfolio or GitHub with previous CV projects preferred."
     ]
   };
+  
 
 }
