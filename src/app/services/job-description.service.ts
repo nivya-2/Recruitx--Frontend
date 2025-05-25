bdb8e55db78375850dd59a72d9cef0ef9191f706
Jjeff2002 <jeffinjitto2002@gmail.com>
2025-05-26 00:49:12 +0530

    'service'

---
+++ src/app/services/job-description.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface JobDescription {
+  id: string;
+  roleTitle: string;
+  deliveryUnit: string;
+  createdDate: string; // Backend returns DateTime; we'll handle format in UI
+  associatedJr: number;
+  status: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobDescriptionService {
+  private apiUrl = '  https://localhost:7144/api/JD'; // <-- Update this
+
+
+  constructor(private http: HttpClient) {}
+
+  getJobDescriptions(): Observable<JobDescription[]> {
+    return this.http.get<JobDescription[]>(this.apiUrl);
+  }
+}
