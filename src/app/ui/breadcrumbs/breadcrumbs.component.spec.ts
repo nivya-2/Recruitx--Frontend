9e9363b01c3322a6e9f14a1ae5f9f3e94e26bad1
Anjalik712 <anjali2sheeja@gmail.com>
2025-06-01 22:29:36 +0530

    fix: remove commented code

--- src/app/ui/breadcrumbs/breadcrumbs.component.spec.ts
+++ src/app/ui/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,27 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import { BreadcrumbsComponent } from './breadcrumbs.component';
-
-// describe('BreadcrumbsComponent', () => {
-//   let component: BreadcrumbsComponent;
-//   let fixture: ComponentFixture<BreadcrumbsComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [BreadcrumbsComponent]
-//     })
-//     .compileComponents();
-
-//     fixture = TestBed.createComponent(BreadcrumbsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
