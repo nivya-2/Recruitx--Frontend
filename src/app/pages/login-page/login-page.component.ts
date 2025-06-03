7934c5048d2e081fbbed6fcbe3790a0bcbce4b55
K_vinj <kevinjoby1234@gmail.com>
2025-06-03 15:53:29 +0530

    Login Page sample

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../ui/button/button.component';
+import { InputTextComponent } from '../../ui/input-text/input-text.component';
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [ButtonComponent, InputTextComponent, PasswordModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+}
