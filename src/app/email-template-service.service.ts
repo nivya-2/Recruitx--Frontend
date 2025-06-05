580ceff687e115dd3f5697510ae79db9634be76e
K_vinj <kevinjoby1234@gmail.com>
2025-06-06 01:58:25 +0530

    Admin-email api call settt

--- src/app/email-template-service.service.ts
+++ src/app/email-template-service.service.ts
@@ -1,6 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+
+export interface EmailTemplate {
+  id: string;
+  name: string;
+  subject: string;
+  body: string;
+  userType: string;
+  category: string;
+  variables?: string[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +19,90 @@ export class EmailTemplateServiceService {
 
   private apiUrl = 'https://localhost:7144/api/EmailTemplates';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // getEmailTemplates(): Observable<any> {
+  //   return this.http.get<any>(this.apiUrl);
+  // }
+
+  getGroupedEmailTemplates(): Observable<Record<string, EmailTemplate[]>> { 
+    
+    return this.http.get<EmailTemplate[]>(this.apiUrl).pipe(
+    map((templates) => {
+      const grouped: Record<string, EmailTemplate[]> = {
+        Candidates: [],
+        Recruiter: [],
+        InterviewPanel: []
+      };
+
+      templates.forEach(t => {
+        // console.log('Processing template:', t);
+        const mappedTemplate = {
+          ...t,
+          variables: t.variables || []
+        };
+
+        if (t.userType === 'Candidate') grouped['Candidates'].push(mappedTemplate);
+        else if (t.userType === 'Recruiter') grouped['Recruiter'].push(mappedTemplate);
+        else if (t.userType === 'InterviewPanel') grouped['InterviewPanel'].push(mappedTemplate);
+      });
+
+      return grouped;
+    })
+  );
+
+  }
+
+  /**
+   * Extract unique variables from all templates
+   */
+  // getAvailableVariables(): Observable<string[]> {
+  //   return this.http.get<EmailTemplate[]>(this.apiUrl).pipe(
+  //     map((templates) => {
+  //       const variableSet = new Set<string>();
+  //       templates.forEach(t => {
+  //         (t.variables || []).forEach(v => variableSet.add(v));
+  //       });
+  //       return Array.from(variableSet);
+  //     })
+  //   );
+  // }
+
+//   classifyEmailTemplates(templates: EmailTemplate[]): Record<string, EmailTemplate[]> {
+//   return templates.reduce((acc, template) => {
+//     const key = template.category; // or template.type
+//     if (!acc[key]) {
+//       acc[key] = [];
+//     }
+//     acc[key].push(template);
+//     return acc;
+//   }, {} as Record<string, EmailTemplate[]>);
+// }
+
 }
+
+
+    // return this.http.get<EmailTemplate[]>(this.apiUrl).pipe(
+    //   map((templates) => {
+    //     const grouped: Record<string, EmailTemplate[]> = {};
+
+    //     templates.forEach(t => {
+    //       const key = t.type;
+    //       const template: EmailTemplate = {
+    //         id: t.id,
+    //         name: t.name,
+    //         subject: t.subject,
+    //         content: t.content,
+    //         type: t.type,
+    //         category: t.type,
+    //         variables: t.variables || []
+    //       };
+
+    //       if (!grouped[key]) grouped[key] = [];
+    //       grouped[key].push(template);
+    //     });
+
+    //     return grouped;
+    //   })
+    // );
+    //
--- src/app/pages/admin-email/admin-email.component.ts
+++ src/app/pages/admin-email/admin-email.component.ts
@@ -4,7 +4,8 @@ import { CardsComponent } from '../../ui/cards/cards.component';
 import { HeaderTextComponent } from '../../ui/header-text/header-text.component';
 import { EmailComponent } from "./email/email.component";
 import { AccordionComponent } from "./accordion/accordion.component";
-import { EmailTemplate } from './email-templates.model';
+// import { EmailTemplate } from './email-templates.model';
+import { EmailTemplate, EmailTemplateServiceService } from '../../email-template-service.service';
 
 
 @Component({
@@ -14,13 +15,31 @@ import { EmailTemplate } from './email-templates.model';
   styleUrl: './admin-email.component.scss'
 })
 export class AdminEmailComponent {
-  
+
+  constructor(private emailTemplateService: EmailTemplateServiceService) { }
+
+  emailTemplates: Record<string, EmailTemplate[]> = {};
+  availableVariables: string[] = [];
+
   selectedTemplate: EmailTemplate | null = null;
-  
+
   onTemplateSelected(template: EmailTemplate): void {
     this.selectedTemplate = template;
+    // this.availableVariables = template.variables || [];
+    // console.log('Available Variables:', this.availableVariables);
   }
 
+  ngOnInit(): void {
+    this.emailTemplateService.getGroupedEmailTemplates().subscribe(grouped => {
+      this.emailTemplates = grouped;
+      // console.log('Email Templates:', this.emailTemplates);
+    });
+
+    // this.emailTemplateService.getAvailableVariables().subscribe(vars => {
+    //   this.availableVariables = vars;
+    //   // console.log('Available Variables:', this.availableVariables);
+    // });
+  }
 
 }
 
