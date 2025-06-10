09fa8cc8f9903e314b00d22d4dfabb585e8a7c86
Anjalik712 <anjali2sheeja@gmail.com>
2025-06-10 10:20:27 +0530

    fix: API integrated for all interviews tab

--- src/app/subpages/shortlist/shortlist.component.ts
+++ src/app/subpages/shortlist/shortlist.component.ts
@@ -49,17 +49,6 @@ export class ShortlistComponent implements OnInit {
   actionMethods={'Shortlist': this.onShortlist };
 
 awaitingShortlistingDataSource: any[] = [];
-    // { id: 'CAN006', name: 'Aswin A', interviewDate: '17/04/2025', interviewType: 'Technical L1' , actions: ['Shortlist']},
-    // { id: 'CAN002', name: 'Priya Sharma', interviewDate: '10/04/2025', interviewType: 'Technical L1' , actions: ['Shortlist']},
-    // { id: 'CAN004', name: 'Aarav Mehta', interviewDate: '09/04/2025', interviewType: 'Technical L1' , actions: ['Shortlist'] },
-    // { id: 'CAN010', name: 'Rohan Kapoor', interviewDate: '17/04/2025', interviewType: 'Technical L2' , actions: ['Shortlist'] },
-    // { id: 'CAN005', name: 'Ananya Iyer', interviewDate: '08/04/2025', interviewType: 'Technical L2' , actions: ['Shortlist'] },
-    // { id: 'CAN003', name: 'Karan Malhotra', interviewDate: '07/04/2025', interviewType: 'Technical L2' , actions: ['Shortlist'] },
-    // { id: 'CAN001', name: 'Vivek Reddy', interviewDate: '06/04/2025', interviewType: 'Management' , actions: ['Shortlist'] },
-    // { id: 'CAN007', name: 'Nisha Patel', interviewDate: '13/04/2025', interviewType: 'Management' , actions: ['Shortlist'] },
-    // { id: 'CAN008', name: 'Arjun Bansal', interviewDate: '04/04/2025', interviewType: 'Management' , actions: ['Shortlist'] }
-  
-
   awaitingColumns: Array<{ key: string, label: string, filterable: boolean,type?:string }> = [
   { key: 'id', label: 'Candidate ID', filterable: false },
   { key: 'jdId', label: 'Job Description ID', filterable: false },
